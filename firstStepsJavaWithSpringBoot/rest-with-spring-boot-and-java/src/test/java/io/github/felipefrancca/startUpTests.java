d2552c97b24f290d2ca9b3a333c980fde67ab0ae
Felipe.Francca <dev.felipefrancca@gmail.com>
2024-07-15 12:07:40 -0300

    firstSteps

---
+++ firstStepsJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/io/github/felipefrancca/startUpTests.java
@@ -0,0 +1,13 @@
+package io.github.felipefrancca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class startUpTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
